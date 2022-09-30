1bf391c7252e720a790d7f8171fcb8c094a28af8
Kenneth Albert <gichuka@gmail.com>
2022-09-30 18:50:35 +0300

    Done

--- app/serializers/author_post_serializer.rb
+++ app/serializers/author_post_serializer.rb
@@ -1,9 +1,9 @@
 class AuthorPostSerializer < ActiveModel::Serializer
-  attributes :title,:short_content
+  attributes :title,:short_content,:tags
   belongs_to :author
-  has_many :tags
 
   def short_content
 "#{self.object.content[0..39]}..."
   end
+ 
 end
